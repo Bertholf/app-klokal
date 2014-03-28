98b9baea8ee9717392f7dfbc23a7512ac441e297
bao <bao_2080@sina.com>
2014-03-28 11:58:00 +0800

    new files

---
+++ app/config/twitter.php
@@ -0,0 +1,7 @@
+<?php
+
+return array(
+	'consumer_key' => 'O0TQdyTYUtPquUzeVIETJQ',
+	'consumer_secret' => 'dGGzxHn0fVZzeioFwbUtKHW1vEHtnTPrvP37kltRDs',
+	'klout_key' => '9sbxzk9g43ka9975dbhzzj5p',
+);
