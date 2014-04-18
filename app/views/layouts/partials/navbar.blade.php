8e149af6063296963239b524c6d0f6c381259a5f
Page Wood <page@pagegwood.com>
2014-04-17 19:55:57 -1000

    format header

--- app/views/layouts/partials/navbar.blade.php
+++ app/views/layouts/partials/navbar.blade.php
@@ -20,43 +20,47 @@
 
 					{{-- Show login items on interior views --}}
 					@if (Auth::check())
-						<div style="text-align: right">
-						<form class="form-inline" id='user-filter' role="form" method='post' action='/change/location'>
-						<div class='col-sm-4' style='padding-right:0px !important; padding-left:0px !important; width: 160px;'>
-		                      <label class="sr-only" for="location">Location</label>
-		                      <select class='form-control' name='current_location'>
-		                        <option value='' disabled selected style='display:none;'>
-		                        <?php if(Session::get('current_location')){
-		                        	$location = Location::where('LocationID', '=', Session::get('current_location'))->first();
-		                        	if(count($location) == 1){
-		                        	 	echo $location->LocationTitle;
-		                        	}else{
-		                        		echo 'Location2';
-		                        	}
-		                        	}else{
-		                        		echo "Location";
-									}
-		                       ?>
-		                        </option>
-		                         @foreach($location_list as $location_key => $location_value)
-		                        <option value="{{$location_value->LocationID}}">{{$location_value->LocationTitle}}</option>
-		                    	 @endforeach
-		                      </select>
-		               </div><!--/.col-sm-3-->
-		               <div class='col-sm-1' style='padding-right:0px !important; padding-left:0px !important;'>
-		                      <button type="submit" class="btn btn-primary btn-block">Change</button>
-		               </div><!--/.col-sm-3-->
-		               </form>
-							<span>Hi, </span>
-							<a href="/user/{{Auth::user()->twitter_handle}}">{{ Auth::user()->name }}</a>
-							<span> | </span>
-							<a href="/logout">Logout</a>
-						</div>
-						@else
-						<div style="text-align: right">
-							<a href="/sign_in_with_twitter">Login</a>
-						</div>
-						@endif
+					<div class='navbar-right'>
+						<div id='logged-in'>
+							<form class="form" id='user-filter' role="form" method='post' action='/change/location'>
+
+			                    <label class="sr-only" for="location">Location</label>
+			                    <div class='input-group'>
+				                    <select class='form-control' name='current_location'>
+				                        <option value='' disabled selected style='display:none;'>
+					                        <?php if(Session::get('current_location')){
+					                        	$location = Location::where('LocationID', '=', Session::get('current_location'))->first();
+					                        	if(count($location) == 1){
+					                        	 	echo $location->LocationTitle;
+					                        	}else{
+					                        		echo 'Location2';
+					                        	}
+					                        	}else{
+					                        		echo "Location";
+												}
+						                    ?>
+				                        </option>
+					                    @foreach($location_list as $location_key => $location_value)
+						                    <option value="{{$location_value->LocationID}}">{{$location_value->LocationTitle}}</option>
+				                    	@endforeach
+				                    </select>
+					               <span class="input-group-btn">
+					                     <button type="submit" class="btn btn-primary">Change</button>
+					               </span><!--/.input-btn-->
+					            </div><!--/.input-group-->
+			                </form>
+			                <div id='user-action'>
+								<span>Hi, </span>
+								<a href="/user/{{Auth::user()->twitter_handle}}">{{ Auth::user()->name }}</a>
+								<a href="/logout"><i class='icon-off'> </i></a>
+
+								@else
+									<a href="/sign_in_with_twitter">Login</a>
+								@endif
+							</div><!--/#user-action-->
+						</div><!--/#loggedin-->
+					</div><!--/.navbar-right-->
+
 
 					@endif
 				</div><!--/.col-sm-12-->
